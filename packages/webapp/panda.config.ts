0cd28810bff58e0f792540f152ff0da1ac732e7c
Islam <islamkwnd@gmail.com>
2024-03-11 15:06:44 +0100

    refactor(webapp): Remove unnecessary parts of config

--- packages/webapp/panda.config.ts
+++ packages/webapp/panda.config.ts
@@ -4,20 +4,16 @@ import { preset } from '@tma/design-system/preset';
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
-  outExtension: 'js',
 
   // Todo: Use a static import for presets.
   presets: ['@pandacss/dev/presets', preset],
 
   // Where to look for your css declarations
   include: ['./app/**/*.{ts,tsx}'],
-  // The output directory for your css system
-  outdir: './src/__generated__',
 
   // The JSX framework to use
   jsxFramework: 'react',
   // The CSS Syntax to use to use
   syntax: 'object-literal',
-  clean: true,
   importMap: '@tma/design-system',
 });
