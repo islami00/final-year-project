06b1352ceb48cd6e871fa2abd4150bda6da74342
Islam <islamkwnd@gmail.com>
2024-03-30 22:48:44 +0100

    refactor(webapp): Remove line-height override as it isn't necessary

--- packages/webapp/app/styles/mantine/mantineComponents.ts
+++ packages/webapp/app/styles/mantine/mantineComponents.ts
@@ -1,5 +1,4 @@
 import { Button, rem, type MantineThemeComponents } from '@mantine/core';
-import { css } from '@tma/design-system';
 
 const ButtonDefaultProps = Button.extend({
   vars: (_, props) => {
@@ -13,19 +12,6 @@ const ButtonDefaultProps = Button.extend({
 
       default:
         return { root: {} };
-    }
-  },
-  classNames(_, props) {
-    switch (props.size) {
-      case 'xs':
-        return {
-          root: css({
-            lineHeight: 1.55,
-          }),
-        };
-
-      default:
-        return {};
     }
   },
 });
