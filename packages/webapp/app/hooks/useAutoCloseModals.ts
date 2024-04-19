febfb1a50a1550f9cf726763e2b0aa198a1fbbdd
Islam <islamkwnd@gmail.com>
2024-04-19 06:58:10 +0100

    docs(modals): Add docs on modal closing

--- packages/webapp/app/hooks/useAutoCloseModals.ts
+++ packages/webapp/app/hooks/useAutoCloseModals.ts
@@ -5,6 +5,7 @@ import { useContext, useEffect } from 'react';
 // Thanks https://github.com/eBay/nice-modal-react/issues/143#issuecomment-1928687042
 /**
  * Auto-closes global modals. Reasons this is fine:
+ * - One of the main ways a modal can be unmounted without closing is by a url change
  * - Global modals should not be connected to the URL as the URl is a global state manager
  * - Thus, auto-closing these when the route changes is valid ux.
  */
