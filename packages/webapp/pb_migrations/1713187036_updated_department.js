1ad83b5c6179d0076f7b0ec04e54245ff83dc001
Islam <islamkwnd@gmail.com>
2024-04-24 08:30:39 +0100

    refactor(migrations): Merge department migrations

--- packages/webapp/pb_migrations/1713187036_updated_department.js
+++ packages/webapp/pb_migrations/1713187036_updated_department.js
@@ -6,6 +6,11 @@ migrate((db) => {
   collection.listRule = "organisationId ?= @request.auth.organisation_users_via_userId.organisationId"
   collection.viewRule = "organisationId ?= @request.auth.organisation_users_via_userId.organisationId"
 
+
+  collection.createRule = "organisationId ?= @request.auth.organisation_users_via_userId.organisationId"
+  collection.updateRule = "organisationId ?= @request.auth.organisation_users_via_userId.organisationId"
+
+
   return dao.saveCollection(collection)
 }, (db) => {
   const dao = new Dao(db)
@@ -13,6 +18,9 @@ migrate((db) => {
 
   collection.listRule = "organisationId = @request.auth.organisation_users_via_userId.organisationId"
   collection.viewRule = "organisationId = @request.auth.organisation_users_via_userId.organisationId"
+
+  collection.createRule = null
+  collection.updateRule = null
 
   return dao.saveCollection(collection)
 })
